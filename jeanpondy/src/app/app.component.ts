46346fdcf43acbf74171f44e84ba4ad8c98ae564
Jean Pondy <manbayesse@gmail.com>
2024-11-02 10:16:00 +0100

    footer component

--- jeanpondy/src/app/app.component.ts
+++ jeanpondy/src/app/app.component.ts
@@ -3,11 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { MainLayoutComponent } from './main-layout/main-layout.component';
 import { ImprintComponent } from './imprint/imprint.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { HeaderComponent } from './main-layout/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MainLayoutComponent, ImprintComponent, PrivacyPolicyComponent],
+  imports: [RouterOutlet, MainLayoutComponent, ImprintComponent, PrivacyPolicyComponent,HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

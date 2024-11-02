1889008a9c653b74ff78302f712ce1ed20972df5
Jean Pondy <manbayesse@gmail.com>
2024-11-02 09:49:50 +0100

    privacy policy component

--- jeanpondy/src/app/app.component.ts
+++ jeanpondy/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainLayoutComponent } from './main-layout/main-layout.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MainLayoutComponent],
+  imports: [RouterOutlet, MainLayoutComponent, ImprintComponent, PrivacyPolicyComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

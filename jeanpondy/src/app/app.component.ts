1e1dd4b191321bff748d58145fda445fae33ad26
Jean Pondy <manbayesse@gmail.com>
2024-11-28 02:28:49 +0100

    update Background

--- jeanpondy/src/app/app.component.ts
+++ jeanpondy/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './shared/footer/footer.component';
 import { TranslateModule } from '@ngx-translate/core';
@@ -9,15 +10,28 @@ import { HeaderComponent } from './main-layout/header/header.component';
   selector: 'app-root',
   standalone: true,
   imports: [
-    CommonModule,       // Für Standard Angular-Direktiven
-    RouterOutlet,       // Zum Hinzufügen der Routing-Funktionalität
-    HeaderComponent,    // Importiert Header, falls verwendet
-    FooterComponent,    // Importiert Footer, falls verwendet
-    TranslateModule     // Falls du Übersetzungen verwendest
+    CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    TranslateModule
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'jeanpondy';
+  showBackground = true; // Steuert die Sichtbarkeit des Hintergrunds
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        // Aktuelle URL in Kleinbuchstaben prüfen
+        const currentUrl = event.urlAfterRedirects.toLowerCase();
+        this.showBackground = !(
+          currentUrl.includes('imprint') || currentUrl.includes('privacy-policy')
+        );
+      }
+    });
+  }
 }
